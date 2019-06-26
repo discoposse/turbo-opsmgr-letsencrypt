0faf204dcbe0a42703af014f618ce779503f8e20
Ryan Geyer <me@ryangeyer.com>
2019-06-26 09:16:14 -0700

    Switch to certbot in a docker container for dependency reasons

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -o errexit -o pipefail -o noclobber -o nounset
+
 if [[ -z "$EMAIL" ]]; then
   echo "The environment variable EMAIL must be set."
   exit 1
@@ -8,53 +11,43 @@ if [[ -z "$DOMAIN" ]]; then
   exit 1
 fi
 
-le_ssl_conf=/etc/httpd/conf.d/00letsencrypt-le-ssl.conf
+# Install docker to run certbot
+curl -fsSL https://get.docker.com/ | sh
 
-# Enable EPEL first
-yum install -y epel-release
-
-# Install certbot
-yum install -y certbot python2-certbot-apache
-
-# Turbonomic OpsMgr doesn't have a default vhost listening on
-# port 80, so we need to create one for certbot to host
-# challenges from
+# Proxy the certbot challenge requests to the certbot docker container
 cat > /etc/httpd/conf.d/00letsencrypt.conf << EOF
-<VirtualHost *:80>
-	DocumentRoot "/var/www/html"
-	ServerName "$DOMAIN"
-</VirtualHost>
-
-RewriteRule ^\.well-known - [L]
+RewriteRule ^/?\.well-known - [L]
+ProxyPass "/.well-known" "http://localhost:8888/.well-known"
 EOF
 
 service httpd reload
 
-certbot --apache --rsa-key-size 4096 -n --agree-tos -m "$EMAIL" -d "$DOMAIN" --redirect
-
-# Once certbot has done it's job, we need to make sure that we're listening on
-# port 443, and that we include the default Turbonomic rewrite rules in the
-# SSL vhost
-if [[ -e "$le_ssl_conf" ]]; then
-        grep "Listen 443" $le_ssl_conf > /dev/null
-        if [[ $? -eq 1 ]]; then
-                echo -e "Listen 443\n$(cat $le_ssl_conf)" > $le_ssl_conf
-        fi
-
-        grep "IncludeOptional" $le_ssl_conf > /dev/null
-        if [[ $? -eq 1 ]]; then
-                sed -i 's,</VirtualHost>,IncludeOptional conf.d/rewrite.conf\n</VirtualHost>,g' $le_ssl_conf
-        fi
-fi
+docker run --rm -it -v /etc/letsencrypt:/etc/letsencrypt -p 8888:80 certbot/certbot \
+  certonly --standalone --rsa-key-size 4096 -n --agree-tos -m "$EMAIL" -d "$DOMAIN"
 
 if [[ -e "/etc/httpd/conf.d/ssl.conf" ]]; then
   mv /etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/ssl.conf.turbo
 fi
 
+cat > "/etc/httpd/conf.d/ssl.conf" << EOF
+Listen 443
+<IfModule mod_ssl.c>
+<VirtualHost *:443>
+	DocumentRoot "/var/www/html"
+	ServerName "$DOMAIN"
+SSLCertificateFile /etc/letsencrypt/live/$DOMAIN/cert.pem
+SSLCertificateKeyFile /etc/letsencrypt/live/$DOMAIN/privkey.pem
+Include /etc/letsencrypt/options-ssl-apache.conf
+SSLCertificateChainFile /etc/letsencrypt/live/$DOMAIN/chain.pem
+IncludeOptional conf.d/rewrite.conf
+</VirtualHost>
+</IfModule>
+EOF
+
 service httpd restart
 
 # Make sure we renew our cert in perpetuity forever
-grep "certbot renew" /etc/crontab > /dev/null
+! grep "certbot renew" /etc/crontab > /dev/null
 if [[ $? -eq 1 ]]; then
-  echo "0 0,12 * * * python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew" | tee -a /etc/crontab > /dev/null
+  echo "0 0,12 * * * docker run --rm -it -v /etc/letsencrypt:/etc/letsencrypt -p 8888:80 certbot/certbot renew --standalone" | tee -a /etc/crontab > /dev/null
 fi
